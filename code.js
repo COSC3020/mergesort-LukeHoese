bc4979c7c6790adb882484aaa8fb2af6a036519b
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-11-26 21:49:12 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
+function mergesorthelper(array, left, middle, rigth) {
+    var x = left;
+    var y = middle + 1;
+
+    while (x <= middle && y <= right) {
+        if (array[x] <= array[y]) {
+            x++;
+        }
+        else {
+            var temp = array[y];
+            var z = y;
+
+            while (z > x) {
+                array[z] = array[z - 1];
+                z--;
+            }
+            array[x] = temp;
+
+            x++;
+            y++;
+            middle++;
+        }
+    }
+}
+
 function mergesort(array) {
+    var length = array.length;
+
+    for (size = 1; size < length; size *= 2) {
+        for (left = 0; left < length - size; left += 2 * size) {
+            var middle = Math.min(left + size - 1, length - 1);
+            var right = Math.min(left + 2 * size - 1, n - 1);
+
+            mergesorthelper(array, left, middle, right);
+        }
+    }
     return array;
 }
